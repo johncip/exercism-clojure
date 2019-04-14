59fe4657e95ef1dd848c9348a5dcb634b34fce50
John Cipriano <johnmikecip@gmail.com>
2019-04-13 21:36:09 -0700

    tweak bob

--- bob/src/bob.clj
+++ bob/src/bob.clj
@@ -1,16 +1,16 @@
 (ns bob)
 (use '[clojure.string :only (blank? upper-case)])
 
-(defn has-letters? [s] (re-find #"[A-Z]" s))
 (defn all-upper? [s] (= s (upper-case s)))
 (defn question? [s] (= (last s) \?))
+(def has-letters? (partial re-find #"[a-zA-Z]"))
 (def yelling? (every-pred has-letters? all-upper?))
-(def yell-question? (every-pred yelling? question?))
+(def loud-question? (every-pred yelling? question?))
 
 (defn response-for [s]
   (cond
     (blank? s)         "Fine. Be that way!"
-    (yell-question? s) "Calm down, I know what I'm doing!"
+    (loud-question? s) "Calm down, I know what I'm doing!"
     (yelling? s)       "Whoa, chill out!"
     (question? s)      "Sure."
     :else              "Whatever."))
