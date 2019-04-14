abb0dd41137a1baac98a9607ced8f2090ff9f991
John Cipriano <johnmikecip@gmail.com>
2019-04-13 21:23:52 -0700

    use blank in bob

--- bob/src/bob.clj
+++ bob/src/bob.clj
@@ -1,16 +1,15 @@
 (ns bob)
-(use '[clojure.string :only (trim upper-case)])
+(use '[clojure.string :only (blank? upper-case)])
 
 (defn has-letters? [s] (re-find #"[A-Z]" s))
 (defn all-upper? [s] (= s (upper-case s)))
-(defn silence? [s] (empty? (trim s)))
 (defn question? [s] (= (last s) \?))
 (def yelling? (every-pred has-letters? all-upper?))
 (def yell-question? (every-pred yelling? question?))
 
 (defn response-for [s]
   (cond
-    (silence? s)       "Fine. Be that way!"
+    (blank? s)         "Fine. Be that way!"
     (yell-question? s) "Calm down, I know what I'm doing!"
     (yelling? s)       "Whoa, chill out!"
     (question? s)      "Sure."
