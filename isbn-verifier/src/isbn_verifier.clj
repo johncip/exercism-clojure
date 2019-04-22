f9b2a009191888d53078d414e6f8c11f2c599cdc
John Cipriano <johnmikecip@gmail.com>
2019-04-22 14:38:38 -0700

    remove doc strings

--- isbn-verifier/src/isbn_verifier.clj
+++ isbn-verifier/src/isbn_verifier.clj
@@ -13,16 +13,12 @@
   (zero? (mod num base)))
 
 (defn to-nums [digits]
-  "Given a string containing nine digits (0-9), plus a tenth which can
-  be a digit or the letter X (for 10), returns a sequence of numbers."
   (let [final (last digits)]
     (conj
       (vec (map char->int (take 9 digits)))
       (if (= final \X) 10 (char->int final)))))
 
 (defn weighted-sum [nums]
-  "Sums the numbers, multiplying each by its position in reverse order:
-  = a * 10 + b * 9 + ..."
   (reduce
     (fn [sum pair]
       (+ (apply * pair) sum))
@@ -30,7 +26,6 @@
     (map vector nums (range 1 11))))
 
 (defn isbn? [isbn]
-  "Returns true if the string is a valid ISBN-10."
   (if-not (valid-format? isbn)
     false
     (-> isbn
