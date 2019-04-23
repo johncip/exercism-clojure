29dc8f8e1cb3ab60f716c914ae993c56b341514d
John Cipriano <johnmikecip@gmail.com>
2019-04-22 18:19:00 -0700

    improve nucleotide-count

--- nucleotide-count/src/nucleotide_count.clj
+++ nucleotide-count/src/nucleotide_count.clj
@@ -1,5 +1,6 @@
 (ns nucleotide-count)
 
+;; original
 (defn nucleotide-counts [strand]
   (merge
     {\A 0 \C 0 \G 0 \T 0}
@@ -8,3 +9,12 @@
 (defn count [tide strand]
   (do (assert (contains? #{\A \C \G \T} tide))
       (get (nucleotide-counts strand) tide 0)))
+
+
+;; better (into > merge, real precondition, don't need get...0)
+(defn nucleotide-counts [strand]
+  (into {\A 0, \T 0, \C 0, \G 0} (frequencies strand)))
+
+(defn count [tide strand]
+  {:pre [(#{\G \A \C \T} tide)]}
+  ((nucleotide-counts strand) tide))
