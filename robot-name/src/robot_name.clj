2153ce8c03f4e0f99ee6b5a2e7c545830bd12771
John Cipriano <johnmikecip@gmail.com>
2019-04-22 21:58:59 -0700

    add robot-name

---
+++ robot-name/src/robot_name.clj
@@ -0,0 +1,21 @@
+(ns robot-name)
+
+(defn random-char []
+  (char (+ (rand 26) 65)))
+
+(defn random-digit []
+  (char (+ (rand 10) 48)))
+
+(defn new-name [& args]
+  (apply str (concat
+               (repeatedly 2 random-char)
+               (repeatedly 3 random-digit))))
+
+(defn robot []
+  (atom (new-name)))
+
+(defn robot-name [robot]
+  @robot)
+
+(defn reset-name [robot]
+  (swap! robot new-name))
