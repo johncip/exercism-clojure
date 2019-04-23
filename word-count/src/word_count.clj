a03e4ee703cc1add1d0655a70fe4a9b4dde590a4
John Cipriano <johnmikecip@gmail.com>
2019-04-22 17:16:00 -0700

    add improved version of word-count

--- word-count/src/word_count.clj
+++ word-count/src/word_count.clj
@@ -1,6 +1,8 @@
 (ns word-count
   (:require [clojure.string :as str]))
 
+
+;; original
 (defn freq [xs]
   (reduce #(merge-with + %1 {%2 1}) {} xs))
 
@@ -9,9 +11,16 @@
       (str/lower-case)
       (str/replace #"\W" "")))
 
-(defn word-count [s]
+(defn word-count-old [s]
   (as-> s t
       (str/split t #"\s+")
       (map normalize t)
       (remove str/blank? t)
       (freq t)))
+
+
+;; improved
+(def words #(re-seq #"\w+" %))
+
+(defn word-count [s]
+  (->> s str/lower-case words frequencies))
