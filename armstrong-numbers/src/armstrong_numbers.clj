ea247903568463482e38af33a6baf13a4c9ca221
John Cipriano <johnmikecip@gmail.com>
2019-04-24 17:23:01 -0700

    improve armstrong-numbers

--- armstrong-numbers/src/armstrong_numbers.clj
+++ armstrong-numbers/src/armstrong_numbers.clj
@@ -1,25 +1,34 @@
 (ns armstrong-numbers)
 
-(defn exp
-  "Returns x raised to n."
-  [x n]
+;; original
+(defn exp [x n]
   (reduce * (repeat n x)))
 
-(defn num_digits [num]
-  "Given a positive number, returns the number of digits."
+(defn num-digits [num]
   (loop [n num res 0]
     (if (zero? n)
       res
       (recur (quot n 10) (inc res)))))
 
-(defn arm_sum [num]
-  "Given a positive number, returns the sum of each digit raised to the number of digits."
+(defn armstrong-sum [num]
   (loop [n num res 0]
     (if (zero? n)
       res
-      (let [nd (num_digits num)]
+      (let [nd (num-digits num)]
         (recur (quot n 10) (+ res (exp (mod n 10) nd)))))))
 
+
+;; better (simpler digit splitting, map & reduce instead of loop)
+(defn digits [n]
+  (map #(Character/digit % 10) (str n)))
+
+(defn exp [b x]
+  (-> b biginteger (.pow x)))
+
+(defn armstrong-sum [num]
+  (let [digs (digits num)
+        raise #(exp %1 (count digs))]
+    (reduce + (map raise digs))))
+
 (defn armstrong? [n]
-  "Returns true if the given positive number is an armstrong number."
-  (= (arm_sum n) n))
+  (= (armstrong-sum n) n))
