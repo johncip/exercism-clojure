5dd796f6e8b2b0fce0bc8741f2d6db2a0e7d62b1
John Cipriano <johnmikecip@gmail.com>
2019-04-24 18:08:27 -0700

    improve series

--- series/src/series.clj
+++ series/src/series.clj
@@ -1,10 +1,21 @@
-(ns series)
+(ns series
+  (:require [clojure.string :as str]))
 
+;; original
 (defn slices [string len]
   (cond
     (zero? len) [""]
     (> len (count string)) []
-    :else (let [last-start (- (count string) len)
-                starts (range 0 (inc last-start))
-                window #(subs string %1 (+ %1 len))]
+    :else
+    (let [last-start (- (count string) len)
+          starts (range 0 (inc last-start))
+          window #(subs string %1 (+ %1 len))]
       (map window starts))))
+
+
+;; better (use parition)
+(defn slices [s len]
+  (cond
+    (zero? len) [""]
+    (> len (count s)) []
+    :else (->> s (partition len 1) (map str/join))))
