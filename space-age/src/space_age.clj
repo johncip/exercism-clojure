f5e6a86debd9b3f6f707c5624d30db0d5bc4f44e
John Cipriano <johnmikecip@gmail.com>
2019-05-01 14:04:44 -0700

    remove a line

--- space-age/src/space_age.clj
+++ space-age/src/space_age.clj
@@ -1,10 +1,9 @@
 (ns space-age)
 
-(def fname->period
-  {'on-earth   1         'on-mercury 0.240846
-   'on-venus   0.615197  'on-mars    1.880815
-   'on-jupiter 11.86261  'on-saturn  29.44749
-   'on-uranus  84.01684  'on-neptune 164.7913})
+(doseq [[fname period]
+        (seq {'on-earth   1         'on-mercury 0.240846
+              'on-venus   0.615197  'on-mars    1.880815
+              'on-jupiter 11.86261  'on-saturn  29.44749
+              'on-uranus  84.01684  'on-neptune 164.7913})]
 
-(doseq [[fname period] (seq fname->period)]
     (intern *ns* fname #(/ % (* period 31557600))))
