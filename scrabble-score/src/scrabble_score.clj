de20746cdfa2fa79dbc7e9c81498761fe9f27c96
John Cipriano <johnmikecip@gmail.com>
2019-05-02 15:38:22 -0700

    add scrabble-score

---
+++ scrabble-score/src/scrabble_score.clj
@@ -0,0 +1,26 @@
+(ns scrabble-score)
+
+(defn invert [multimap]
+  (into {} (mapcat
+             (fn [[k v]] (map vector (seq v) (repeat k)))
+             multimap)))
+
+(def values-by-letter
+  (invert {1 "AEIOULNRST"
+           2 "DG"
+           3 "BCMP"
+           4 "FHVWY"
+           5 "K"
+           8 "JX"
+           10 "QZ"}))
+
+(defn score-letter [letter]
+  (-> letter
+      (#(if (string? %) (first %) %))
+      (#(Character/toUpperCase %))
+      values-by-letter))
+
+(defn score-word [word]
+  (reduce + (map score-letter word)))
+
+(scrabble-score/score-word "OXYPHENBUTAZONE")
