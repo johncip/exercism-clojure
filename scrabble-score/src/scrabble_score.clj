7d2826a94b90c551ba3e8d5a5c2279b35179191e
John Cipriano <johnmikecip@gmail.com>
2019-05-02 15:53:40 -0700

    improve scrabble-score

--- scrabble-score/src/scrabble_score.clj
+++ scrabble-score/src/scrabble_score.clj
@@ -1,24 +1,24 @@
-(ns scrabble-score)
+(ns scrabble-score
+  (:require [clojure.string :as str]))
 
-(defn invert [multimap]
+(defn invert [split-fn multimap]
   (into {} (mapcat
-             (fn [[k v]] (map vector (seq v) (repeat k)))
+             (fn [[k v]] (map vector (split-fn v) (repeat k)))
              multimap)))
 
-(def values-by-letter
-  (invert {1 "AEIOULNRST"
-           2 "DG"
-           3 "BCMP"
-           4 "FHVWY"
-           5 "K"
-           8 "JX"
-           10 "QZ"}))
+(def letter-scores
+  (invert
+    #(str/split % #"")
+    {1 "AEIOULNRST"
+     2 "DG"
+     3 "BCMP"
+     4 "FHVWY"
+     5 "K"
+     8 "JX"
+     10 "QZ"}))
 
-(defn score-letter [letter]
-  (-> letter
-      (#(if (string? %) (first %) %))
-      (#(Character/toUpperCase %))
-      values-by-letter))
+(def score-letter
+  (comp letter-scores str/upper-case))
 
 (defn score-word [word]
   (reduce + (map score-letter word)))
