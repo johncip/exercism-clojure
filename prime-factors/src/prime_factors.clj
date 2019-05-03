f55581d274f0e13cf570d82a8447d017193ee60e
John Cipriano <johnmikecip@gmail.com>
2019-05-03 15:44:07 -0700

    tweak prime-factors

--- prime-factors/src/prime_factors.clj
+++ prime-factors/src/prime_factors.clj
@@ -1,8 +1,5 @@
 (ns prime-factors)
 
-(defn multiple? [a b]
-  (zero? (mod a b)))
-
 (def divisors
   (concat '(2) (iterate (partial + 2) 3)))
 
@@ -12,8 +9,7 @@
            [d & ds :as divs] divisors
            factors []]
       (cond
-        (= n 1)         factors
-        (> d root)      (conj factors n) ; just a speedup
-        (multiple? n d) (recur (/ n d) divs (conj factors d))
-        :else           (recur n ds factors)
-      ))))
+        (= n 1)           factors
+        (> d root)        (conj factors n) ; just a speedup
+        (zero? (mod n d)) (recur (/ n d) divs (conj factors d))
+        :else             (recur n ds factors)))))
