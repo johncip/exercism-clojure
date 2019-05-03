d5054c538c8d2d4758fa9f0e99540d6cddf23fe4
John Cipriano <johnmikecip@gmail.com>
2019-05-02 17:44:40 -0700

    improve roman-numerals

--- roman-numerals/src/roman_numerals.clj
+++ roman-numerals/src/roman_numerals.clj
@@ -1,8 +1,7 @@
 (ns roman-numerals)
 
 (def arabic->roman
-  (sorted-map-by
-    (comp - compare)
+  (sorted-map-by >
     1000 "M" 900 "CM" 500 "D" 400 "CD"
      100 "C"  90 "XC"  50 "L"  40 "XL"
       10 "X"   9 "IX"   5 "V"   4 "IV"
