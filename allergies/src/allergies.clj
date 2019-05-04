5b612c1a0e3acade6d509b8510bdf111be30d7e6
John Cipriano <johnmikecip@gmail.com>
2019-05-03 19:17:36 -0700

    use keep-indexed in allergies

--- allergies/src/allergies.clj
+++ allergies/src/allergies.clj
@@ -1,16 +1,14 @@
 (ns allergies)
 
 (def allergens
-  [:eggs :peanuts :shellfish
-   :strawberries :tomatoes
-   :chocolate :pollen :cats])
+  [:eggs :peanuts :shellfish :strawberries
+   :tomatoes :chocolate :pollen :cats])
 
-(defn on? [pow n]
-  (-> (bit-shift-left 1 pow) (bit-and n) pos?))
+(defn on? [i n]
+  (-> (bit-shift-left 1 i) (bit-and n) pos?))
 
 (defn allergies [n]
-  (filter some?
-    (map-indexed #(if (on? %1 n) %2) allergens)))
+  (keep-indexed #(if (on? %1 n) %2) allergens))
 
 (defn allergic-to? [n gen]
-  (contains? (set (allergies n)) gen))
+  ((set (allergies n)) gen))
