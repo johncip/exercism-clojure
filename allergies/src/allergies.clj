9913406af690c391df431ad37df4c2577cec2cd7
John Cipriano <johnmikecip@gmail.com>
2019-05-03 19:21:43 -0700

    use bit-test in allergies

--- allergies/src/allergies.clj
+++ allergies/src/allergies.clj
@@ -4,11 +4,8 @@
   [:eggs :peanuts :shellfish :strawberries
    :tomatoes :chocolate :pollen :cats])
 
-(defn on? [i n]
-  (-> (bit-shift-left 1 i) (bit-and n) pos?))
-
 (defn allergies [n]
-  (keep-indexed #(if (on? %1 n) %2) allergens))
+  (keep-indexed #(if (bit-test n %1) %2) allergens))
 
 (defn allergic-to? [n gen]
   ((set (allergies n)) gen))
