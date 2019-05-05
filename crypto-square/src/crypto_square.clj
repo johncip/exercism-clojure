cd85ab826c8350cdf31058e9693bee851fa19825
John Cipriano <johnmikecip@gmail.com>
2019-05-04 19:17:22 -0700

    golf crypto_square

--- crypto-square/src/crypto_square.clj
+++ crypto-square/src/crypto_square.clj
@@ -1,29 +1,25 @@
 (ns crypto-square
   (:require [clojure.string :as str]))
 
-(defn clean [s]
+(defn normalize-plaintext [s]
   (->> s str/lower-case (re-seq #"\w") str/join))
-(def normalize-plaintext clean)
 
 (defn square-size [s]
-  (-> s clean count Math/sqrt Math/ceil int))
+  (-> s normalize-plaintext count Math/sqrt Math/ceil int))
 
-(defn segments [s]
+(defn- segments [s]
   (let [n (square-size s)]
     (partition n n (repeat nil) s)))
 
-(defn join-each [coll]
-  (map str/join coll))
+(defn plaintext-segments [s]
+  (map str/join (segments (normalize-plaintext s))))
 
-(def plaintext-segments
-  (comp join-each segments clean))
-
-(def rotated
+(defn- rotated [s]
   (let [rotate #(apply mapv vector %)]
-    (comp join-each rotate segments)))
+    (map str/join (rotate (segments s)))))
 
 (defn ciphertext [s & delim]
-  (str/join (first delim) (rotated (clean s))))
+  (str/join (first delim) (rotated (normalize-plaintext s))))
 
 (defn normalize-ciphertext [s]
   (ciphertext s " "))
