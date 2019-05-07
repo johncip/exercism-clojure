26089bc1405a17b71d6ed434956960210bc884b1
John Cipriano <johnmikecip@gmail.com>
2019-05-07 00:41:23 -0700

    improve sum_of_multiples

--- sum-of-multiples/src/sum_of_multiples.clj
+++ sum-of-multiples/src/sum_of_multiples.clj
@@ -4,8 +4,6 @@
   (some #(zero? (rem n %)) divs))
 
 (defn sum-of-multiples [divs n]
-  (loop [sum 0, n (dec n)]
-    (cond
-      (zero? n) sum
-      (some-multiple? divs n) (recur (+ sum n) (dec n))
-      :else (recur sum (dec n)))))
+  (->> (range 0 n)
+       (filter (partial some-multiple? divs))
+       (reduce +)))
