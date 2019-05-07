d2be67f333033bed9d2c57f5ec03f0ef1e202e51
John Cipriano <johnmikecip@gmail.com>
2019-05-07 00:44:22 -0700

    improve sum_of_multiples

--- sum-of-multiples/src/sum_of_multiples.clj
+++ sum-of-multiples/src/sum_of_multiples.clj
@@ -1,9 +1,17 @@
 (ns sum-of-multiples)
 
+;; original
 (defn some-multiple? [divs n]
   (some #(zero? (rem n %)) divs))
 
 (defn sum-of-multiples [divs n]
   (->> (range 0 n)
        (filter (partial some-multiple? divs))
+       (reduce +)))
+
+;; better
+(defn sum-of-multiples [divs n]
+  (->> divs
+       (mapcat (fn [step] (range 0 n step)))
+       distinct
        (reduce +)))
