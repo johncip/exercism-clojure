f53a3fc1a69b9e77ee80276655a4b74ae40c8e5f
John Cipriano <johnmikecip@gmail.com>
2019-05-07 23:42:44 -0700

    improve sieve

--- sieve/src/sieve.clj
+++ sieve/src/sieve.clj
@@ -1,13 +1,17 @@
 (ns sieve)
 
+(defn irange [a b & args]
+  (apply range (concat [a (inc b)] args)))
+
 (defn sieve [lim]
-  (let [max (inc lim)
-        nums (atom (vec (range 0 max)))
-        mark #(swap! nums assoc % nil)]
+  (let [nums (atom (vec (irange 0 lim)))
+        mark #(swap! nums assoc % nil)
+        root (int (Math/sqrt lim))]
     (loop [i 2]
-      (let [cur (nth @nums i nil)]
+      (let [cur (nth @nums i nil)
+            i' (inc i)]
         (cond
-          (> i lim)  (drop 2 (filter some? @nums))
-          (nil? cur) (recur (inc i))
-          :else      (do (doall (map mark (range (* i 2) max i)))
-                         (recur (inc i))))))))
+          (> i root)  (drop 2 (filter some? @nums))
+          (nil? cur)  (recur i')
+          :else       (do (doall (map mark (irange (* i i) lim i)))
+                          (recur i')))))))
