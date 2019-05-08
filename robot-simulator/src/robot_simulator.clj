794b04e8e537fdbe67242a74ada6c02dfed8f4f0
John Cipriano <johnmikecip@gmail.com>
2019-05-08 15:21:07 -0700

    add robot_simulator

---
+++ robot-simulator/src/robot_simulator.clj
@@ -0,0 +1,26 @@
+(ns robot-simulator)
+
+(defn robot [pos dir]
+  {:coordinates pos :bearing dir})
+
+(def moves
+  {:north {:x 0 :y  1} :east {:x  1 :y 0}
+   :south {:x 0 :y -1} :west {:x -1 :y 0}})
+
+(defn advance [bot]
+  (let [{pos :coordinates, dir :bearing} bot]
+    (assoc bot :coordinates
+      (merge-with + pos (moves dir)))))
+
+(defn turn-right [dir]
+  (let [dirs (cycle [:north :east :south :west])]
+    (first (next (drop-while #(not= dir %) dirs)))))
+
+(defn turn-left [dir]
+  (nth (iterate turn-right dir) 3))
+
+(defn simulate [codes bot]
+  (let [ops {\A advance
+             \R #(update % :bearing turn-right)
+             \L #(update % :bearing turn-left)}]
+    (reduce #(%2 %1) bot (map ops codes))))
