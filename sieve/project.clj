6d914db645bcfd536e8f2753a80e7eea107718f5
John Cipriano <johnmikecip@gmail.com>
2019-05-07 22:41:27 -0700

    add sieve

---
+++ sieve/project.clj
@@ -0,0 +1,5 @@
+(defproject sieve "0.1.0-SNAPSHOT"
+  :description "sieve exercise."
+  :url "https://github.com/exercism/clojure/tree/master/exercises/sieve"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/math.combinatorics "0.1.5"]])
---
+++ sieve/src/sieve.clj
@@ -0,0 +1,14 @@
+(ns sieve)
+
+(defn sieve [lim]
+  (let [max (inc lim)
+        nums (atom (vec (range 0 max)))
+        mark #(swap! nums assoc % nil)]
+    (loop [i 2]
+      (let [cur (nth @nums i nil)]
+        (cond
+          (> i lim)  (drop 2 (filter some? @nums))
+          (nil? cur) (recur (inc i))
+          :else      (do (doall (map mark (range (* i 2) max i)))
+                         (recur (inc i)))
+          )))))
