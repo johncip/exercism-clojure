2830d65c44178273a62744c715a7c70d67c75397
John Cipriano <johnmikecip@gmail.com>
2019-05-08 01:29:04 -0700

    tweak luhn

--- luhn/src/luhn.clj
+++ luhn/src/luhn.clj
@@ -8,15 +8,20 @@
   (and (re-matches #"[\d ]+" s)
        (> (count (digits s)) 1)))
 
+(defn double-every-other [ds]
+  (letfn [(dub [n] (- (* 2 n) (if (< n 5) 0 9)))]
+    (map-indexed
+      (fn [i d] (if (even? i) d (dub d)))
+      ds)))
+
 (defn valid-digits? [s]
-  (letfn [(dub9 [n] (- (* 2 n) (if (< n 5) 0 9)))]
-    (->> s
-         digits
-         reverse
-         (map-indexed #(if (even? %1) %2 (dub9 %2)))
-         (reduce +)
-         (#(mod % 10))
-         zero?)))
+  (->> s
+       digits
+       reverse
+       double-every-other
+       (reduce +)
+       (#(mod % 10))
+       zero?))
 
 (def valid?
   (every-pred valid-format? valid-digits?))
