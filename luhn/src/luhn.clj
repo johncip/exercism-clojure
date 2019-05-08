d816959938312feb953276a9a3c8b336577b8bd7
John Cipriano <johnmikecip@gmail.com>
2019-05-08 01:37:06 -0700

    bikeshed luhn

--- luhn/src/luhn.clj
+++ luhn/src/luhn.clj
@@ -1,18 +1,18 @@
 (ns luhn)
 
 (defn digits [s]
-  (map #(Character/digit (first %) 10)
-       (re-seq #"\d" s)))
+  (map read-string (re-seq #"\d" s)))
 
 (defn valid-format? [s]
   (and (re-matches #"[\d ]+" s)
        (> (count (digits s)) 1)))
 
 (defn double-every-other [ds]
-  (letfn [(dub [n] (- (* 2 n) (if (< n 5) 0 9)))]
-    (map-indexed
-      (fn [i d] (if (even? i) d (dub d)))
-      ds)))
+  (map-indexed
+    (fn [i d] (cond (even? i) d
+                    (< d 5)   (* 2 d)
+                    :else     (- (* 2 d) 9)))
+    ds))
 
 (defn valid-digits? [s]
   (->> s
