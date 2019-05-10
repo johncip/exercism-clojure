04ecfcfd95ab8533df939c4c436fcebc984472f0
John Cipriano <johnmikecip@gmail.com>
2019-05-10 03:32:09 -0700

    further golf queen_attack

--- queen-attack/src/queen_attack.clj
+++ queen-attack/src/queen_attack.clj
@@ -1,19 +1,19 @@
 (ns queen-attack
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :refer [join]]))
 
 (defn index [[row col]]
   (+ (* 8 row) col))
 
 (defn board-string [mp]
   (if (empty? mp)
-    (str/join (repeat 8 "_ _ _ _ _ _ _ _\n"))
+    (join (repeat 8 "_ _ _ _ _ _ _ _\n"))
     (let [w (index (:w mp))
           b (index (:b mp))]
       (->> (range 0 64)
            (mapv #({w \W b \B} % \_))
            (partition 8)
-           (map #(str (str/join " " %) "\n"))
-           str/join))))
+           (map #(str (join " " %) "\n"))
+           join))))
 
 (defn can-attack [{w :w b :b}]
   (let [disp (comp #(Math/abs %) -)
