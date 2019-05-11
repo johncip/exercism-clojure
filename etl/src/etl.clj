972cf5b3285ec816677a8f65c9128bc17c8fd8ce
John Cipriano <johnmikecip@gmail.com>
2019-05-11 16:22:28 -0700

    improve etl

--- etl/src/etl.clj
+++ etl/src/etl.clj
@@ -1,8 +1,21 @@
 (ns etl
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :refer [lower-case]]))
 
+;; original version
 (defn split-entry [[score words]]
-  (map #(vector (str/lower-case %) score) words))
+  (map #(vector (lower-case %) score) words))
 
 (defn transform [source]
   (into {} (mapcat split-entry source)))
+
+;; zipmap version
+(defn transform [mp]
+  (let [repeat-key #(repeat (count (mp %)) %)
+        keys'      (flatten (vals mp))
+        vals'      (mapcat repeat-key (keys mp))]
+    (zipmap (map lower-case keys') vals')))
+
+;; for version
+(defn transform [mp]
+  (into {} (for [k (keys mp), v (mp k)]
+             [(lower-case v) k])))
