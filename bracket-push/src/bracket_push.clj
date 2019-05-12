93971de4cc1dc0079c87aad299b43765e3c48416
John Cipriano <johnmikecip@gmail.com>
2019-05-11 23:36:11 -0700

    reformat bracket_push

--- bracket-push/src/bracket_push.clj
+++ bracket-push/src/bracket_push.clj
@@ -3,14 +3,14 @@
 (def right-for (zipmap "([{" ")]}"))
 (def left? (set (keys right-for)))
 
-(defn brackets [s]
-  (filter #{ \( \) \[ \] \{ \} } s))
-
 (defn on-stack [[k & ks :as stack] c]
   (cond
     (left? c) (conj stack (right-for c))
     (= c k)   ks
     :else     (reduced '($))))
+
+(defn brackets [s]
+  (filter #{ \( \) \[ \] \{ \} } s))
 
 (defn valid? [s]
   (empty? (reduce on-stack '() (brackets s))))
