f972161dad489c41e85e1d3e1e0d4b68533583eb
John Cipriano <johnmikecip@gmail.com>
2019-05-13 14:23:40 -0700

    add nth_prime

---
+++ nth-prime/src/nth_prime.clj
@@ -0,0 +1,24 @@
+(ns nth-prime)
+
+(defn sieve [lim]
+  (if (< lim 2) '()
+    (let [nums (atom (vec (range 0 (inc lim))))
+          mark #(swap! nums assoc % nil)
+          sqrt (Math/sqrt lim)]
+      (loop [i 2]
+        (let [cur (@nums i)
+              doomed (range (* i i) (inc lim) i)
+              i' (inc i)]
+          (cond
+            (> i sqrt)  (drop 2 (filter some? @nums))
+            (nil? cur)  (recur i')
+            :else       (do (doall (map mark doomed))
+                            (recur i'))))))))
+
+;; the nth prime is ~ n ln n
+(defn upper-bound [n]
+  (+ 2 (int (* 4/3 n (Math/log n)))))
+
+(defn nth-prime [n]
+  (when-not (pos? n) (throw (IllegalArgumentException.)))
+  (nth (sieve (upper-bound n)) (dec n)))
