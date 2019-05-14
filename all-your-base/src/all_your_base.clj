08c0220b711377d195e50deaed1c8ab498b57a2c
John Cipriano <johnmikecip@gmail.com>
2019-05-13 20:08:04 -0700

    add all_your_base

---
+++ all-your-base/src/all_your_base.clj
@@ -0,0 +1,22 @@
+(ns all-your-base)
+
+(defn valid-conversion? [base1 digits base2]
+  (and
+    (seq digits)
+    (not-any? neg? digits)
+    (every? #(> % 1) [base1 base2])
+    (every? #(< % base1) digits)))
+
+(defn to-decimal [base digits]
+  (reduce #(+ (* %1 base) %2) digits))
+
+(defn to-digits [base n]
+  (if (< n base) [n]
+    (conj (to-digits base (quot n base))
+          (mod n base))))
+
+(defn convert [base1 digits base2]
+  (if (valid-conversion? base1 digits base2)
+    (->> digits
+         (to-decimal base1)
+         (to-digits base2))))
