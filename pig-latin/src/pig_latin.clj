a8348b5810904cec6271140944dc4a5fb7159412
John Cipriano <johnmikecip@gmail.com>
2019-05-13 17:44:47 -0700

    add pig_latin

--- pig-latin/src/pig_latin.clj
+++ pig-latin/src/pig_latin.clj
@@ -1,26 +1,21 @@
 (ns pig-latin
   (:require [clojure.string :as str]))
 
-(defn boundary-matches [word]
-  (re-matches
-    (re-pattern
-      (str/join "|"
-        ["([aeiou].*)"
-         "(xr.*)"
-         "(yt.*)"
-         "([bcdfghjklmnpqrstvwxz])(y)"
-         "([bcdfghjklmnprstvwxyz]*qu)(.*)"
-         "([bcdfghjklmnpqrstvwxyz]+)(.*)"
-         ])) word))
+(def patterns
+  [#"(xr.*)"
+   #"(yt.*)"
+   #"([aeiou].*)"
+   #"([bcdfghjklmnpqrstvwxz])(y)"
+   #"([bcdfghjklmnprstvwxyz]*qu)(.*)"
+   #"([bcdfghjklmnpqrstvwxyz]+)(.*)"])
 
-(defn split [word]
-  (drop 1 (filter some? (boundary-matches word))))
+(defn parts [word]
+  (->> (some #(re-matches % word) patterns)
+       (remove nil?)
+       rest))
 
 (defn say [word]
-  (-> (split word)
-      reverse
-      str/join
-      (str "ay")))
+  (str/join (concat (reverse (parts word)) ["ay"])))
 
 (defn translate [words]
   (->> (str/split words #"\s")
