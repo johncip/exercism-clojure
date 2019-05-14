014f49bec2b84ee9cc0207a3d8eb110329efe908
John Cipriano <johnmikecip@gmail.com>
2019-05-14 16:05:00 -0700

    shorten binary_search_tree

--- binary-search-tree/src/binary_search_tree.clj
+++ binary-search-tree/src/binary_search_tree.clj
@@ -1,32 +1,21 @@
 (ns binary-search-tree)
 
 (defn singleton [value]
-  {:value value})
+  [nil value nil])
 
-(doseq [k [:left :right :value]]
-  (intern *ns* (symbol k) k))
+(def left first)
+(def right last)
+(def value second)
 
-;; definitely not self-balancing
+;; definitely not balanced...
 (defn insert [v bst]
   (if (nil? bst)
     (singleton v)
     (condp apply [v (value bst)]
-      <= (assoc bst :left (insert v (left bst)))
-       > (assoc bst :right (insert v (right bst))))))
+      <= (assoc bst 0 (insert v (left bst)))
+       > (assoc bst 2 (insert v (right bst))))))
 
-;; there must be a better way
-(defn to-list [bst]
-  (let [l (left bst)
-        r (right bst)
-        v (value bst)]
-    (cond
-      (not (or l r)) [v]
-      (not l)        (concat [v] (to-list r))
-      (not r)        (concat (to-list l) [v])
-      :else          (concat (to-list l) [v] (to-list r)))))
+(defn from-list [[x & xs]]
+  (reduce #(insert %2 %1) (singleton x) xs))
 
-(defn from-list [ls]
-  (reduce #(insert %2 %1) (singleton (first ls)) (rest ls)))
-
-;; (from-list (shuffle (range 1 6)))
-;; (to-list (from-list (shuffle (range 1 6))))
+(def to-list (comp #(remove nil? %) flatten))
