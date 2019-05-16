676eea73521067a4ea91336fc31d6580f8863849
John Cipriano <johnmikecip@gmail.com>
2019-05-16 00:37:06 -0700

    add diamond

---
+++ diamond/src/diamond.clj
@@ -0,0 +1,18 @@
+(ns diamond
+  (:require [clojure.string :as str]))
+
+(defn letter [i]
+  (char (+ i (int \A))))
+
+(defn mirror [xs]
+  (concat xs (rest (reverse xs))))
+
+(defn line [len i]
+  (-> (vec (repeat len \space))
+      (assoc (- len i 1) (letter i))
+      mirror
+      str/join))
+
+(defn diamond [c]
+  (let [len (inc (- (int c) (int \A)))]
+    (mirror (map #(line len %) (range len)))))
