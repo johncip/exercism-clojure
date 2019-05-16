16c3963e40e7588bd1e5f843caae3e44211376aa
John Cipriano <johnmikecip@gmail.com>
2019-05-16 01:02:17 -0700

    golf diamond

--- diamond/src/diamond.clj
+++ diamond/src/diamond.clj
@@ -1,18 +1,15 @@
-(ns diamond
-  (:require [clojure.string :as str]))
-
-(defn letter [i]
-  (char (+ (int \A) i)))
+(ns diamond)
 
 (defn mirror [xs]
   (concat xs (rest (reverse xs))))
 
 (defn line [len i]
-  (-> (vec (repeat len \space))
-      (assoc (- len i 1) (letter i))
+  (-> (vec (repeat len " "))
+      (assoc (- len i 1) (char (+ 65 i)))
       mirror
-      str/join))
+      clojure.string/join))
 
 (defn diamond [c]
   (let [len (inc (- (int c) (int \A)))]
-    (mirror (map #(line len %) (range len)))))
+    (mirror
+      (map #(line len %) (range len)))))
