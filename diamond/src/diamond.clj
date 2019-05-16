a4356ac683280e2501156bb0b9bee39d67cf2de6
John Cipriano <johnmikecip@gmail.com>
2019-05-16 01:14:20 -0700

    make diamond readable again

--- diamond/src/diamond.clj
+++ diamond/src/diamond.clj
@@ -1,15 +1,22 @@
-(ns diamond)
+(ns diamond
+  (:require [clojure.string :as str]))
+
+(defn letter [i]
+  (char (+ (int \A) i)))
 
 (defn mirror [xs]
   (concat xs (rest (reverse xs))))
 
+(defn line-length [c]
+  (inc (- (int c) (int \A))))
+
 (defn line [len i]
-  (-> (vec (repeat len " "))
-      (assoc (- len i 1) (char (+ 65 i)))
+  (-> (vec (repeat len \space))
+      (assoc (- len i 1) (letter i))
       mirror
-      clojure.string/join))
+      str/join))
 
 (defn diamond [c]
-  (let [len (inc (- (int c) (int \A)))]
+  (let [len (line-length c)]
     (mirror
       (map #(line len %) (range len)))))
