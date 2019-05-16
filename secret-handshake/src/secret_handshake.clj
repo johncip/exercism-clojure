c5bd2558fcb0bd43ce7bbc8ba7738a4bd87b9ac5
John Cipriano <johnmikecip@gmail.com>
2019-05-15 21:13:12 -0700

    golf secret_handshake

--- secret-handshake/src/secret_handshake.clj
+++ secret-handshake/src/secret_handshake.clj
@@ -1,7 +1,6 @@
 (ns secret-handshake)
 
 (defn commands [n]
-  (reduce
-    (fn [res [k v]] (if (bit-test n k) (conj res v) res))
-    (if (bit-test n 4) '() [])
-    {0 "wink" 1 "double blink" 2 "close your eyes" 3 "jump"}))
+  ((if (bit-test n 4) reverse identity)
+   (keep-indexed #(if (bit-test n %1) %2)
+     ["wink" "double blink" "close your eyes" "jump"])))
