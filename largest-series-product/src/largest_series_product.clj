d7609c2e8720e8ed7b90e1c2c72f13f8af11daf3
John Cipriano <johnmikecip@gmail.com>
2019-05-17 18:58:41 -0700

    tweak largest_series_product

--- largest-series-product/src/largest_series_product.clj
+++ largest-series-product/src/largest_series_product.clj
@@ -1,14 +1,12 @@
 (ns largest-series-product)
 
-(def digit #(Character/digit % 10))
-
 (defn largest-product [n ds]
   {:pre [(<= 0 n (count ds))
          (re-matches #"\d*" ds)]}
   (cond
     (zero? n) 1
     (empty? ds) 1
-    :else (->> (map digit ds)
+    :else (->> (map #(Character/digit % 10) ds)
                (partition n 1)
                (map #(apply * %))
                sort
