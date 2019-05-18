3536f3e14c09b9ed178b70e38722c20ca078deb5
John Cipriano <johnmikecip@gmail.com>
2019-05-17 19:01:31 -0700

    golf largest_series_product

--- largest-series-product/src/largest_series_product.clj
+++ largest-series-product/src/largest_series_product.clj
@@ -3,11 +3,9 @@
 (defn largest-product [n ds]
   {:pre [(<= 0 n (count ds))
          (re-matches #"\d*" ds)]}
-  (cond
-    (zero? n) 1
-    (empty? ds) 1
-    :else (->> (map #(Character/digit % 10) ds)
-               (partition n 1)
-               (map #(apply * %))
-               sort
-               last)))
+  (if (zero? n) 1
+    (->> (map #(Character/digit % 10) ds)
+         (partition n 1)
+         (map #(apply * %))
+         sort
+         last)))
