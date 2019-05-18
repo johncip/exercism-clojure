bc7f594085464a77e46336dd25ca9af7868c28af
John Cipriano <johnmikecip@gmail.com>
2019-05-17 17:47:20 -0700

    refactor minesweeper

--- minesweeper/src/minesweeper.clj
+++ minesweeper/src/minesweeper.clj
@@ -1,43 +1,37 @@
 (ns minesweeper
   (:require [clojure.string :refer [split join]]))
 
-(defn put-when [coll k v]
-  (if (contains? coll k) (assoc coll k v) coll))
-
-(defn get-g [grid row col]
-  (nth (nth grid row nil) col nil))
-
-(defn update-g [grid row col f]
-  (let [old (get-g grid row col)
-        new (f old)]
-    (put-when grid row
-         (put-when (nth grid row nil) col new))))
-
-(defn coords [r0 r1 c0 c1]
-  (let [rs (range r0 (inc r1))
-        cs (range c0 (inc c1))]
-    (for [r rs c cs] (vector r c))))
-
-(defn inc-square [v]
-  (cond (= v \*) \*
-        (= v \space) 1
-        (number? v) (inc v)))
-
-(defn mark-around [board [row col]]
-  (let [v (get-g board row col)]
-    (if-not (= v \*)
-      board
-      (reduce
-        (fn [b [r c]] (update-g b r c inc-square))
-        board
-        (coords (dec row) (inc row) (dec col) (inc col))))))
-
-(defn normalize [board]
+(defn to-grid [board]
   (mapv #(apply vector %) (split board #"\n")))
 
+(defn positions [r0 rn c0 cn]
+  (let [rows (range r0 (inc rn))
+        cols (range c0 (inc cn))]
+    (for [r rows c cols] (list r c))))
+
+(def plus
+  (zipmap (concat (range 1 10) [\* \space])
+          (concat (range 2 11) [\* 1])))
+
+(defn inc-in [grid pos]
+  (if (get-in grid pos)
+    (update-in grid pos plus)
+    grid))
+
+(defn mark-around [grid [r c]]
+  (let [v (get-in grid [r c])
+        window (positions (dec r) (inc r) (dec c) (inc c))]
+    (if (= v \*)
+      (reduce inc-in grid window)
+      grid)))
+
+(defn sweep [b]
+  (let [each-pos (positions 0 (count b) 0 (count (first b)))]
+    (reduce mark-around b each-pos)))
+
 (defn draw [board]
-  (let [b (normalize board)
-        coords (coords 0 (count b) 0 (count (first b)))]
-    (->> (reduce mark-around b coords)
-         (map join)
-         (join "\n"))))
+  (->> board
+       to-grid
+       sweep
+       (map join)
+       (join "\n")))
