c50265c7e2531899661fbd398a95cb9bf789975a
John Cipriano <johnmikecip@gmail.com>
2019-05-23 23:04:18 -0700

    reformat change

--- change/src/change.clj
+++ change/src/change.clj
@@ -1,18 +1,16 @@
 (ns change)
 
-(def helper
-  (memoize
-    (fn [amt coins]
-      (let [[c & cs] coins
-            d (second coins)]
-        (cond
-          (zero? amt)    []
-          (empty? coins) []
-          (empty? cs)    (conj (helper (- amt c) coins) c)
-          (> c amt)      (helper amt cs)
-          :else          (min-key count
-                           (conj (helper (- amt c) coins) c)
-                           (conj (helper (- amt d) cs) d)))))))
+(def helper (memoize (fn [amt coins]
+  (let [[c & cs] coins
+        d (second coins)]
+    (cond
+      (zero? amt)    []
+      (empty? coins) []
+      (empty? cs)    (conj (helper (- amt c) coins) c)
+      (> c amt)      (helper amt cs)
+      :else          (min-key count
+                       (conj (helper (- amt c) coins) c)
+                       (conj (helper (- amt d) cs) d)))))))
 
 (defn issue [amt coins]
   (let [err (IllegalArgumentException. "cannot change")]
