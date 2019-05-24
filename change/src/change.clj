3aea7945811d1ef08f02a1289355e344420fc137
John Cipriano <johnmikecip@gmail.com>
2019-05-23 23:05:13 -0700

    rename helper -> search in change

--- change/src/change.clj
+++ change/src/change.clj
@@ -1,20 +1,20 @@
 (ns change)
 
-(def helper (memoize (fn [amt coins]
+(def search (memoize (fn [amt coins]
   (let [[c & cs] coins
         d (second coins)]
     (cond
       (zero? amt)    []
       (empty? coins) []
-      (empty? cs)    (conj (helper (- amt c) coins) c)
-      (> c amt)      (helper amt cs)
+      (empty? cs)    (conj (search (- amt c) coins) c)
+      (> c amt)      (search amt cs)
       :else          (min-key count
-                       (conj (helper (- amt c) coins) c)
-                       (conj (helper (- amt d) cs) d)))))))
+                       (conj (search (- amt c) coins) c)
+                       (conj (search (- amt d) cs) d)))))))
 
 (defn issue [amt coins]
   (let [err (IllegalArgumentException. "cannot change")]
     (cond
       (zero? amt) []
       (< amt (apply min coins)) (throw err)
-      :else (helper amt (sort > coins)))))
+      :else (search amt (sort > coins)))))
