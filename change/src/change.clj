8c9dee4ce7c5758eb35b83ec95638f5779783f92
John Cipriano <johnmikecip@gmail.com>
2019-05-23 23:09:34 -0700

    golf change

--- change/src/change.clj
+++ change/src/change.clj
@@ -4,17 +4,15 @@
   (let [[c & cs] coins
         d (second coins)]
     (cond
-      (zero? amt)    []
-      (empty? coins) []
-      (empty? cs)    (conj (search (- amt c) coins) c)
-      (> c amt)      (search amt cs)
-      :else          (min-key count
-                       (conj (search (- amt c) coins) c)
-                       (conj (search (- amt d) cs) d)))))))
+      (zero? amt) []
+      (empty? cs) (conj (search (- amt c) coins) c)
+      (> c amt)   (search amt cs)
+      :else       (min-key count
+                    (conj (search (- amt c) coins) c)
+                    (conj (search (- amt d) cs) d)))))))
 
 (defn issue [amt coins]
-  (let [err (IllegalArgumentException. "cannot change")]
-    (cond
-      (zero? amt) []
-      (< amt (apply min coins)) (throw err)
-      :else (search amt (sort > coins)))))
+  (if (and (not (zero? amt))
+           (< amt (apply min coins)))
+    (throw (IllegalArgumentException. "cannot change"))
+    (search amt (sort > coins))))
