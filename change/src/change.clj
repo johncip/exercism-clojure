52ebf89e984a4ac4b0d657cbe859df2858d85c29
John Cipriano <johnmikecip@gmail.com>
2019-05-23 21:17:26 -0700

    reformat change

--- change/src/change.clj
+++ change/src/change.clj
@@ -9,13 +9,13 @@
       (let [[c & cs] coins
             c2 (second coins)]
         (cond
-          (zero? amt) []
+          (zero? amt)    []
           (empty? coins) []
-          (> c amt) (helper amt cs)
-          (empty? cs) (conj (helper (- amt c) coins) c)
-          :else (smaller
-                  (conj (helper (- amt c) coins) c)
-                  (conj (helper (- amt c2) cs) c2)))))))
+          (empty? cs)    (conj (helper (- amt c) coins) c)
+          (> c amt)      (helper amt cs)
+          :else          (smaller
+                           (conj (helper (- amt c) coins) c)
+                           (conj (helper (- amt c2) cs) c2)))))))
 
 (defn issue [amt coinset]
   (let [err (IllegalArgumentException. "cannot change")]
