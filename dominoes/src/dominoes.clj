cb9f21c21e3d0886f7da8973b73e98562742a20c
John Cipriano <johnmikecip@gmail.com>
2019-06-17 20:42:12 -0700

    tweak dominoes

--- dominoes/src/dominoes.clj
+++ dominoes/src/dominoes.clj
@@ -1,48 +1,46 @@
 (ns dominoes)
 
-(defn can-play? [[a b] [c d]]
+(defn match? [[a b] [c d]]
   (or (nil? a) (= b c)))
 
-;; see if tiles can be played in the given order,
-;; allowing only rotation. returns the correctly
-;; rotated list, or nil.
+;; checks if tiles can be played in the given order,
+;; allowing only rotation. returns the rotated tiles if so.
 (defn valid-middle [tiles]
-  (letfn [(play [played [t & ts :as tiles]]
+  (letfn [(search [path [t & ts :as tiles]]
             (let [rt (reverse t)
-                  tail (last played)
-                  continue #(play (conj played %) ts)]
+                  tail (last path)
+                  continue #(search (conj path %) ts)]
               (cond
-                (empty? tiles)      played
-                (empty? played)     (or (continue t)
-                                        (continue rt))
-                (can-play? tail t)  (continue t)
-                (can-play? tail rt) (continue rt)
+                (empty? tiles)   path
+                (empty? path)    (or (continue t) (continue rt))
+                (match? tail t)  (continue t)
+                (match? tail rt) (continue rt)
                 :else nil)))]
-    (play [] tiles)))
+    (search [] tiles)))
 
+;; returns true if tiles can be played in the given order,
+;; and first & last number are equal.
 (defn valid? [tiles]
   (let [soln (valid-middle tiles)
         llast (comp last last)]
     (and soln
          (= (ffirst soln) (llast soln)))))
 
-;; use of frequencies is technically just a speedup
 (defn without [freqs t]
   (let [n (dec (freqs t))]
     (if (zero? n)
       (dissoc freqs t)
       (assoc freqs t n))))
 
-(defn solve [soln freqs]
-  (if (empty? freqs)
-    (when (valid? soln) soln)
-    (filter seq
-      (map
-        #(when (valid-middle soln)
-           (solve (conj soln %) (without freqs %)))
-        (keys freqs)))))
-
+;; returns each valid order (inc. rotations) that tiles can be played in.
+;; backtracking dfs.
 (defn can-chain? [tiles]
-  (if (empty? tiles)
-    true
-    (seq (solve [] (frequencies tiles)))))
+  (letfn [(solve [soln freqs]
+            (if (empty? freqs)
+              (when (valid? soln) soln)
+              (keep seq (map
+                          #(when (valid-middle soln)
+                             (solve (conj soln %) (without freqs %)))
+                          (keys freqs)))))]
+  (or (empty? tiles)
+      (seq (solve [] (frequencies tiles))))))
